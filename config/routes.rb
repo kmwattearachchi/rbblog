a37ce81b4d14a0096c639ae1bea4399f061a2388
kalana <kalana@intervest.gi>
2018-04-05 12:44:59 +0530

    master : added partials

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
 
   get :autocomplete,  controller: :page
   get :search,  controller: :page
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
